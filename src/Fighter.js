732c4dbad1f5488f9841918da4d80c3b052a44b6
Burdon Jordan <burdon.jordan@gmail.com>
2025-05-19 12:19:42 +0200

    Atelier finished

--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,27 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+export class Fighter {
+	constructor(name, strength, dexterity) {
+		this.name = name;
+		this.strength = strength;
+		this.dexterity = dexterity;
+		this.life = MAX_LIFE;
+	}
+
+	fight(fighter) {
+		const damagePoints = Math.floor(Math.random() * this.strength + 1);
+		const damageDealt = damagePoints - fighter.dexterity;
+		if (damageDealt > 0) {
+			if (fighter.life - damageDealt <= 0) {
+				fighter.life = 0;
+			} else {
+				fighter.life -= damageDealt;
+			}
+		}
+	}
+
+	isAlive() {
+		return this.life > 0;
+	}
+}
